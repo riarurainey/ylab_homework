a82f37bdd6cb45c4f6687318792c2c7a17ca1059
riarurainey <riarurainey@yandex.ru>
2023-03-05 19:21:05 +0300

    Initial commit

---
+++ src/PellNumbers.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PellNumbers {
+
+    public static void main(String[] args) {
+        readNumber();
+    }
+
+    private static void readNumber() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            System.out.println(getPellNumbers(n));
+
+        } catch (InputMismatchException e) {
+            System.out.println("Неверный формат ввода. Нужно ввести число.");
+        }
+    }
+
+    private static int getPellNumbers(int n) {
+        if (n <= 2) {
+            return n;
+        } else {
+            return 2 * getPellNumbers(n - 1) + getPellNumbers(n - 2);
+        }
+    }
+}
+
